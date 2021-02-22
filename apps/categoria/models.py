329aaf88af510111ab67bdaff2322868efff61c2
Jeremias C. Santacruz <jeresanta@gmail.com>
2021-02-22 11:40:40 -0300

    Agregada los templates de lista categoria

--- apps/categoria/models.py
+++ apps/categoria/models.py
@@ -5,5 +5,8 @@ class categoria(models.Model):
     nombre = models.CharField(max_length=25)
     desc = models.CharField(max_length=60)
 
+    class Meta:
+        db_table = 'categoria'
+
     def __str__(self):
         return self.nombre
