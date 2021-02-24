dca760fd3452821612abe83eac2054edb8825ee3
Jeremias C. Santacruz <jeresanta@gmail.com>
2021-02-24 11:22:02 -0300

    Refactoreando

--- apps/categoria/urls.py
+++ apps/categoria/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path 
-from .views import  crearCategoria, updateCategoria, listaCategoria, detalleCategoria, deleteCategoria
+from django.urls import path
+from .views import (CrearCategoria, DeleteCategoria, DetalleCategoria,
+                    ListaCategoria, UpdateCategoria)
 
 app_name="categoria"
 
 urlpatterns = [
-    path('', listaCategoria.as_view(), name='lista'),
-   # path('nuevo/', crearCategoria.as_view(), name='crear' ), 
-   # path('<int:pk>/update', updateCategoria.as_view(), name='actualizar'),
-   # path('<int:pk>/delete', deleteCategoria.as_view(), name='eliminar'),
-    path('<int:pk>', detalleCategoria.as_view(), name='detalle'),
+    path('', ListaCategoria.as_view(), name='lista'),
+    #path('nuevo/', CrearCategoria.as_view(), name='crear' ), 
+    #path('<int:pk>/update', UpdateCategoria.as_view(), name='actualizar'),
+    #path('<int:pk>/delete', DeleteCategoria.as_view(), name='eliminar'),
+    path('<int:pk>', DetalleCategoria.as_view(), name='detalle'),
 ]
--- infoFinal/settings.py
+++ infoFinal/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'apps.categoria',
 ]
 
+# User model
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
