6e883560372528623106fc16e0c36cb22d4ba7d9
Jeremias C. Santacruz <jeresanta@gmail.com>
2021-03-01 08:43:54 -0300

    Agregado Navbar y fix algunos type errors

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 # Create your models here.
 class Usuario(AbstractUser):
@@ -11,3 +12,9 @@ class Usuario(AbstractUser):
     def __str__(self):
         return self.first_name + self.last_name
     
+
+    def get_absolute_url(self):
+        """
+        Obtengo la url del objeto
+        """
+        return reverse('usuario:perfil', kwargs={'pk':self.pk} )
