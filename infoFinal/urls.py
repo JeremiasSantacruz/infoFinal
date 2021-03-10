f55a589ba97f492e9a3615350d9461d8d2101394
Jeremias C. Santacruz <jeresanta@gmail.com>
2021-03-10 15:11:43 -0300

    Solucionado error en navbar

--- infoFinal/urls.py
+++ infoFinal/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin/', admin.site.urls ),
     path('categoria/', include('apps.categoria.urls')),
     path('user/', include('apps.usuario.urls')),
     path('', include('apps.publicacion.urls')),
